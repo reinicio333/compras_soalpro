8616861922022482040a04c4bfa292e12cfd1d10
bismartlimamanseda <bismartlimamanseda@gmail.com>
2026-03-03 10:46:59 -0400

    limpieza de desayuno

--- orion/Recursos/archivos_orden.sql
+++
@@ -1,7 +0,0 @@
--- En este enfoque NO se usa tabla hija.
--- Se agrega una columna JSON a orden_compra para almacenar array de adjuntos.
-IF COL_LENGTH('orden_compra', 'rutas_archivos') IS NULL
-BEGIN
-    ALTER TABLE orden_compra
-    ADD rutas_archivos NVARCHAR(MAX) NULL;
-END
